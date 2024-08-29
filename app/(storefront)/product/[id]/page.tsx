0afca348096b9813c733e6941d12d4722bf14256
rindah <ngwanyam@gmail.com>
2024-08-29 10:57:38 +0100

    fixed price

--- app/(storefront)/product/[id]/page.tsx
+++ app/(storefront)/product/[id]/page.tsx
@@ -45,7 +45,7 @@ export default async function ProductIdRoute({
           <h1 className="text-3xl font-extrabold tracking-tight text-gray-900">
             {data.name}
           </h1>
-          <p className="text-3xl mt-2 text-gray-900">${data.price}</p>
+          <p className="text-3xl mt-2 text-gray-900">{data.price.toLocaleString()} CFA</p>
           <div className="mt-3 flex items-center gap-1">
             <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
             <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
