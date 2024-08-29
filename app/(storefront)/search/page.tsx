edb72039a52bd13e3ad573ba67bc6de67972cf73
rindah <ngwanyam@gmail.com>
2024-08-29 16:35:08 +0100

    search functionality

--- app/(storefront)/search/page.tsx
+++ app/(storefront)/search/page.tsx
@@ -1,5 +1,7 @@
 import { ProductCard } from "@/app/components/storefront/ProductCard";
 import prisma from "@/app/lib/db";
+import { Hero } from "@/app/components/storefront/Hero";
+import { SearchBar } from "@/app/components/storefront/SearchBar";
 
 export default async function SearchPage({
                                              searchParams,
@@ -27,16 +29,20 @@ export default async function SearchPage({
 
     return (
         <div>
-            <h1 className="text-2xl font-bold mb-4">Search Results for "{query}"</h1>
-            {products.length === 0 ? (
-                <p>No products found.</p>
-            ) : (
-                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-                    {products.map((product) => (
-                        <ProductCard key={product.id} item={product} />
-                    ))}
-                </div>
-            )}
+            <Hero />
+            <SearchBar />
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <h1 className="text-2xl font-bold mb-4">Search Results for "{query}"</h1>
+                {products.length === 0 ? (
+                    <p>No products found.</p>
+                ) : (
+                    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+                        {products.map((product) => (
+                            <ProductCard key={product.id} item={product} />
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
